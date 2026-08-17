b0166fad9379a62e87689424b1699ec895d1f5ec
aborin-png <aborin@bostondyanamics.com>
2026-08-17 15:17:35 -0400

    Changed Version number to 7.2

--- Automations_GUI/UI_Handler.py
+++ Automations_GUI/UI_Handler.py
@@ -39,7 +39,7 @@ CAMERA_CHANNELS = glossary.CAMERA_CHANNELS  # robot Zone ID -> 1-based NVR chann
 ZONE_NAMES = glossary.ZONE_NAMES
 
 VERSION_NUMBER_MAJOR = 7
-VERSION_NUMBER_MINOR = 1
+VERSION_NUMBER_MINOR = 2
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
